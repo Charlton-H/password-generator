2b61a1d1d1d81a332fe0ddf6a675bf3adfa47362
Charlie H <charltonhua@gmail.com>
2021-11-20 03:40:53 -0800

    finalized

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,61 +21,129 @@ generateBtn.addEventListener("click", writePassword);
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
 
-function passwordLength() {
-  var prompPasswordtLength = window.prompt(
-    "Please enter a number for the desired character length of password, between 8 and 128."
+var upperChars = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowerChars = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChars = "~`!#$%^&*+=-[]\\';,/{}|\":<>?";
+
+function generatePassword() {
+  // "^\\d+$"
+  var regEx = RegExp("^[0-9]*$");
+
+  var passwordLength = prompt(
+    "Please enter the number of characters you want for you new password, between 8 and 128."
   );
 
-  var regEx = RegExp("^\\d+$");
   if (
-    regEx.test(prompPasswordtLength) &&
-    prompPasswordtLength >= 8 &&
-    prompPasswordtLength <= 128
+    regEx.test(passwordLength) &&
+    passwordLength >= 8 &&
+    passwordLength <= 128
   ) {
-    var isValidLength = prompPasswordtLength;
-    lowerCaseType();
-  } // or just: /^\d+$/.test(strNumber);
-  window.alert(
-    "You need to provide a valid numeric answer between 8 and 128! Please try again."
-  );
-  return passwordLength();
+    alert("Please confirm the following desired outcomes.");
+  } else {
+    window.alert(
+      "You need to provide a valid numeric answer between 8 and 128! Please try again."
+    );
+    return generatePassword();
+  }
 
-  // if (promptLength === "" || promptLength === null) {
-  //   window.alert(
-  //     "You need to provide a valid answer between 8 and 128! Please try again."
-  //   );
-  //   return passwordLength();
-  // } else if (promptLength !== "[0-9]" || "8" < promptLength > "128") {
-  //   window.alert("You need to provide a valid number! Please try again.");
-  //   passwordLength();
-  // } else {
-  //   // if (promptLength === "[0-9]" && "8" > passwordLength.length < "128")
-  //   return passwordLength();
-  // }
+  var numbers = confirm("Do you want numbers in your password?");
+
+  var lowerCases = confirm("Do you want lowercases in your password?");
+
+  var upperCases = confirm("Do you want uppercases in your password?");
+
+  var special = confirm("Do you want special characters in your password?");
+
+  var possibleValues = "";
+
+  if (numbers) {
+    possibleValues += nums.join("");
+  }
+
+  if (lowerCases) {
+    possibleValues += lowerChars.join("");
+  }
+
+  if (upperCases) {
+    possibleValues += upperChars.join("");
+  }
+
+  if (special) {
+    possibleValues += specialChars;
+  }
+
+  // empty string variable for the for loop below
+  var randomPasswordGenerated = "";
+  // console.log(stringValues);
+
+  // loop getting random characters
+  for (var i = 0; i < passwordLength; i++) {
+    randomPasswordGenerated += possibleValues.charAt(
+      Math.floor(Math.random() * possibleValues.length)
+    );
+  }
+  // to make sure characters are added to the password
+
+  return randomPasswordGenerated;
 }
 
 // WHEN asked for character types to include in the password
 // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-function lowerCaseType() {
-  var promptLowerCaseType = window.prompt(
-    "Would you like to include lowercase characters?"
-  );
-
-  var promptLowerCaseType = promptLowerCaseType.toLowerCase();
-
-  if (promptLowerCaseType === "" || promptLowerCaseType === null) {
-    window.alert(
-      'You need to provide a valid "Yes" or "No"! Please try again.'
-    );
-    lowerCaseType();
-  } else if (promptLowerCaseType === "yes") {
-    var hasLowerCaseType = true;
-    console.log(hasLowerCaseType);
-  } else {
-    var hasLowerCaseType = false;
-    console.log(hasLowerCaseType);
-  }
-}
 
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
@@ -85,7 +153,3 @@ function lowerCaseType() {
 
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
-
-function generatePassword() {
-  passwordLength();
-}
